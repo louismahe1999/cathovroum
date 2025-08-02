d645ad8fd6658480fc36790ca64d9b835a6dbb93
louismahe1999 <mahelouis1999@gmail.com>
2025-08-02 18:49:00 +0200

    Add files via upload

--- cathovroum/app/layout.tsx
+++ cathovroum/app/layout.tsx
@@ -1,17 +1,14 @@
-import './globals.css';
-import { ClerkProvider } from '@clerk/nextjs';
+import './globals.css'
 
 export const metadata = {
   title: 'CathoVroum',
   description: 'Covoiturage entre catholiques',
-};
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <html lang="fr">
-        <body>{children}</body>
-      </html>
-    </ClerkProvider>
-  );
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
 }
