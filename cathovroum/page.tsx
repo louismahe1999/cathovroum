637ce798c3ab301a50707a534714b06e911d8885
louismahe1999 <mahelouis1999@gmail.com>
2025-08-02 18:55:57 +0200

    Delete cathovroum directory

--- cathovroum/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem' }}>
-      <h1>CathoVroum fonctionne 🎉</h1>
-      <p>Page d’accueil sans Clerk, test de base</p>
-    </main>
-  );
-}
